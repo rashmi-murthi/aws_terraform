060ac1e8ce6d7967e0878e2f1fa1e160944af2e2
Rashmi Murthi <rashmi.pmurthi20@gmail.com>
2025-11-02 04:43:50 +0000

    userdata and eip

--- EC2/main.tf
+++ EC2/main.tf
@@ -5,9 +5,14 @@ resource "aws_instance" "web_server" {
   tags = {
     Name = "web-server"
   }
+  user_data = file("install-nginx.sh")
 }
 #New key pair
 /* resource "aws_key_pair" "new_key" {
    key_name = var.key_name
    public_key = file(var.public_key_path)
  }*/
+ resource "aws_eip" "web_eip" {
+  instance = aws_instance.web_server.id
+  domain   = "vpc" 
+}
